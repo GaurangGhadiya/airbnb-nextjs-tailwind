3e80e08c616c559f013dd52a7b3014c4862d7351
GaurangGhadiya <gaurangghadiya99@gmail.com>
2022-05-04 00:21:53 +0530

    header and banner

---
+++ components/Banner.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+
+const Banner = () => {
+  return (
+    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[650px]">
+      <Image
+        src="https://links.papareact.com/0fm"
+        layout="fill"
+        objectFit="cover"
+      />
+      <div className="absolute top-1/2 text-center w-full">
+        <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+        <button className="text-purple-500 bg-white rounded-full shadow-md py-4 px-10 font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
+          I'am flexible
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
